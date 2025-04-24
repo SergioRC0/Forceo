fab741e5f631f490e03be8e3682563d085d751c9
TudorRI <tudorrauliuga6@gmail.com>
2025-04-24 09:58:51 +0200

    Ver posts?

--- backend/app/index.js
+++ backend/app/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 const authRoutes = require('../routes/auth');
+const postRoutes = require('./routes/posts');
+const cors = require('cors');
 
 app.use(express.json());
 app.use('/api/auth', authRoutes);
-
-const cors = require('cors');
 app.use(cors()); // <- habilita peticiones desde otros orígenes
+app.use('/api/posts', postRoutes);
+
 
 app.listen(3000, () => {
   console.log('Servidor corriendo en http://localhost:3000');
